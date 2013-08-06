7f8b922b0b201bae6b1d3f6c8c248e14b6b6a2f1
Mark Friedenbach <mark@friedenbach.org>
2013-08-06 13:53:39 -0700

    [DiffAdjust] Special, one-time adjustment of difficulty due to the "hash crash" of Apr/May 2013 which rushed the introduction of the new difficulty adjustment filter. The difficulty is adjusted back to the difficulty prior to most recent adjustment at that time.

--- src/main.cpp
+++ src/main.cpp
@@ -1340,6 +1340,12 @@ unsigned int GetNextWorkRequired(const CBlockIndex* pindexLast, const CBlockHead
     if (pindexLast == NULL)
         return nProofOfWorkLimit;
 
+    // Special, one-time adjustment due to the "hash crash" of Apr/May 2013
+    // which rushed the introduction of the new difficulty adjustment filter.
+    // We adjust back to the difficulty prior to the last adjustment.
+    if (!(TestNet() || RegTest()) && pindexLast->nHeight==(Params().DiffAdjustThreshold()-1))
+        return 0x1b01c13a;
+
     bool fUseFilter = (pindexLast->nHeight >= (Params().DiffAdjustThreshold()-1));
 
     int64_t nInterval       = nFilteredInterval;
