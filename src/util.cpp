36accf73fd78202f0fecbb015cf81847d09b8373
Mark Friedenbach <mark@friedenbach.org>
2013-08-06 13:26:21 -0700

    Change Bitcoin genesis block for Freicoin on both mainnet and testnet, and update checkpoints.

--- src/util.cpp
+++ src/util.cpp
@@ -1227,7 +1227,7 @@ const boost::filesystem::path &GetDataDir(bool fNetSpecific)
         path = GetDefaultDataDir();
     }
     if (fNetSpecific && GetBoolArg("-testnet", false))
-        path /= "testnet3";
+        path /= "testnet";
 
     fs::create_directories(path);
 
