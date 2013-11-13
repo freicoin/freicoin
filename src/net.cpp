cb5d28bd5b99c55ab45b0ba7e323d1c58417c0bd
joeswhite <joeswhite@gmail.com>
2013-11-13 16:25:23 -0500

    Update net.cpp
    updated max outbound connections to 25. 8 connections is too low for a small network. 25 connections were suggested by Mark Friedenbach

--- src/net.cpp
+++ src/net.cpp
@@ -27,7 +27,7 @@
 using namespace std;
 using namespace boost;
 
-static const int MAX_OUTBOUND_CONNECTIONS = 8;
+static const int MAX_OUTBOUND_CONNECTIONS = 25;
 
 bool OpenNetworkConnection(const CAddress& addrConnect, CSemaphoreGrant *grantOutbound = NULL, const char *strDest = NULL, bool fOneShot = false);
 
