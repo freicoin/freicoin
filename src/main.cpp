ee92a8a8449649b5d127eb4dd376b71e3061dde8
Mark Friedenbach <mark@friedenbach.org>
2018-03-28 12:21:34 -0700

    Add GetTimeAdjustedValue_apu_inner, an integer-math-only method for performing demurrage calculations. Note that due to truncation of the intermediate multiplication terms it is possible for this method to disagree with the results of the MPFR calculation by as much as 1 kria, even after the latter is truncated. It boasts a 10x improvement in speed, however.

--- src/main.cpp
+++ src/main.cpp
@@ -1835,6 +1835,155 @@ const CTxOut &CTransaction::GetOutputFor(const CTxIn& input, CCoinsViewCache& vi
     return coins.vout[input.prevout.n];
 }
 
+static int64_t GetTimeAdjustedValue_apu_inner(int64_t value, unsigned relative_depth)
+{
+    /* This array of pre-generated constants is an exponentiation
+     * ladder of properly calculated 64-bit fixed point demurrage
+     * rates for power-of-2 block intervals. Calculating the actual
+     * demurrage rate for the passed in relative_depth is a matter of
+     * performing fixed point multiplication of the factors
+     * corresponding to the powers of 2 (set bits) which make up
+     * relative_depth.
+     *
+     * Our lookup table does not go beyond 26 entries because a
+     * relative_depth of 1<<26 (the would-be 27th entry) would cause
+     * even MAX_MONEY (2^53 - 1) to decay to zero. */
+    static std::array<uint32_t, 2*26> k32 = {
+        0xfffff000UL, 0x00000000UL, /* 2^0 = 1 */
+        0xffffe000UL, 0x01000000UL, /* 2^1 = 2 */
+        0xffffc000UL, 0x05ffffc0UL, /* 2^2 = 4 */
+        0xffff8000UL, 0x1bfffc80UL, /* 2^3 = 8 */
+        0xffff0000UL, 0x77ffdd00UL, /* ... */
+        0xfffe0001UL, 0xeffeca00UL,
+        0xfffc0007UL, 0xdff5d409UL,
+        0xfff8001fUL, 0xbfaca8a2UL,
+        0xfff0007fUL, 0x7d5d5a6aUL,
+        0xffe001feUL, 0xeacb48a8UL,
+        0xffc007fdUL, 0x55dfda2aUL,
+        0xff801ff6UL, 0xad5499cdUL,
+        0xff007fcdUL, 0x67f98aadUL,
+        0xfe01fe9bUL, 0x74f0943eUL,
+        0xfc07f540UL, 0x767d2a82UL,
+        0xf81fab16UL, 0x3dc15990UL,
+        0xf07d5f65UL, 0xf9604ac9UL,
+        0xe1eb5045UL, 0x80b6ebf7UL,
+        0xc75f7b66UL, 0xa5075defUL,
+        0x9b459576UL, 0x663bbb3eUL,
+        0x5e2d55e7UL, 0x48e27ab4UL,
+        0x22a5531dUL, 0x29a95916UL,
+        0x04b054d7UL, 0xfda49c4dUL,
+        0x0015fc1bUL, 0x85085be9UL,
+        0x000001e3UL, 0x54ca043cUL,
+        0x00000000UL, 0x00039089UL };
+
+    /* The demurrage rate for a depth of 0, which is 1.0 exactly, has
+     * no representation in 0.64 fixed point. */
+    if (relative_depth == 0)
+        return value;
+    /* Depth of (1<<26) and beyond are sufficient to decay even
+     * MAX_MONEY to zero. */
+    if (relative_depth >= (1<<26))
+        return 0;
+
+    /* Overflow sensitive fixed point multiply-and-accumulate methods
+     * which are used both for the exponentiation to calcuate the
+     * demurrage rate, and the final multiply at the end. */
+    uint64_t sum, overflow;
+    auto shift32 = [&]() {
+        sum = (overflow << 32) + (sum >> 32);
+        overflow = 0;
+    };
+    auto term = [&](uint64_t val) {
+        overflow += (sum + val) < sum;
+        sum += val;
+    };
+
+    /* We calculate the first 64 fractional bits of the demurrage rate
+     * for relative_depth by raising the per-block rate of (1 - 2^-20)
+     * to the relative_depth power. To perform this computation
+     * efficiently we perform N multiplications of a pre-computed
+     * exponentiation ladder, where N is the number of set bits in the
+     * binary representation of relative_depth. */
+
+    /* At the end of this calculation w will contain the first 64 bits
+     * of the demurrage rate as a pair of 32-bit unsigned words, the
+     * most significant word first. Its initial value is
+     * multiplicative identity, 1.0, for which the fractional bits are
+     * zero. */
+    std::array<uint32_t, 2> w = { };
+
+    /* The first multiplication has the accumulator set to 1.0, which
+     * is the only time it has a value >= 1. This means that there are
+     * some terms which are non-zero on only the very first
+     * multiplication performed, and since the fractional bits of 1.0
+     * are zero, the other terms that would normally are used are
+     * not. */
+    bool first = true;
+
+    for (int bit = 0; relative_depth; relative_depth >>= 1, ++bit) {
+        if (relative_depth & 1) {
+            /* Zero-extend the accumulator state and ladder entry as
+             * we will be doing our multiplications in 64-bit to
+             * capture to full range. */
+            const uint64_t w0 = w[0];
+            const uint64_t w1 = w[1];
+            const uint64_t k0 = k32[2*bit];
+            const uint64_t k1 = k32[2*bit+1];
+
+            /* Carry out the multiplication, term-by-term. Terms whose
+             * contribution to the final result are entirely wiped
+             * away by truncation are not included. */
+
+            sum = k1;
+            overflow = 0;
+            if (!first) {
+                sum *= w0;
+                term(k0 * w1);
+                shift32();
+                term(k0 * w0);
+            }
+            w[1] = static_cast<uint32_t>(sum);
+            shift32();
+
+            if (first) {
+                term(k0);
+            }
+            w[0] = static_cast<uint32_t>(sum);
+
+            /* Debugging check: under no circumstances should it ever
+             * be the case that the high-order bits of sum (including
+             * detected overflow) are non-zero. That would indicate
+             * that the multiplication resulted in a value of 1.0 or
+             * greater, which shouldn't be possile. */
+            // shift32();
+            // assert(sum == 0);
+
+            first = false;
+        }
+    }
+
+    /* We now perform an approximately similar multiplication of the
+     * final calculated demurrage factor by the passed in value. */
+    const uint64_t v0 = static_cast<uint32_t>(value);
+    const uint64_t v1 = value >> 32;
+
+    overflow = 0;
+    sum = (w[1] * v0) >> 32;
+    term(w[1] * v1);
+    term(w[0] * v0);
+    shift32();
+    term(w[0] * v1);
+
+    /* Debugging check: having the overflow bit set at this point
+     * would indicate that the demurrage calculation has resulted in
+     * an amount that is greater than std::numeric_limits<int64_t>::
+     * max(), which should never be possible as the demurrage factor
+     * should always be a fractional number less than one. */
+    // assert(overflow == 0);
+
+    return sum;
+}
+
 mpq GetTimeAdjustedValue(int64 nInitialValue, int nRelativeDepth)
 {
     return GetTimeAdjustedValue(i64_to_mpq(nInitialValue), nRelativeDepth);
