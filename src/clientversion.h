d0793dd4f80ae522e9098806614267f0caa4d1fc
Mark Friedenbach <mark@friedenbach.org>
2018-04-29 13:02:16 -0700

    [Version] Add a fourth version field, the patch number, to be incremented for each release made against the same upstream version. The not-commonly-used build number becomes a monotonically increasing release identifier.

--- src/clientversion.h
+++ src/clientversion.h
@@ -12,6 +12,7 @@
 #define CLIENT_VERSION_MAJOR       0
 #define CLIENT_VERSION_MINOR       9
 #define CLIENT_VERSION_REVISION    5
+#define CLIENT_VERSION_PATCH       0
 #define CLIENT_VERSION_BUILD       0
 
 // Set to true for release, false for prerelease or test build
@@ -19,7 +20,7 @@
 
 // Copyright year (2009-this)
 // Todo: update this when changing our copyright comments in the source
-#define COPYRIGHT_YEAR 2015
+#define COPYRIGHT_YEAR 2018
 
 #endif //HAVE_CONFIG_H
 
