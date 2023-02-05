6887d5f014df61ef6114426c2933ad4f08d1421b
mowjoejoejoejoe <124041561+mowjoejoejoejoe@users.noreply.github.com>
2023-02-05 14:45:09 -0600

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,708 @@
-usr/bin/.sh env :
+Skip to content
+Search or jump to…
+Pull requests
+Issues
+Codespaces
+Marketplace
+Explore
+ 
+@mowjoejoejoejoe 
+Your account has been flagged.
+Because of that, your profile is hidden from the public. If you believe this is a mistake, contact support to have your account status reviewed.
+mowjoejoejoejoe
+/
+WORKSFLOW
+Public
+Fork your own copy of mowjoejoejoejoe/WORKSFLOW
+Code
+Issues
+5
+Pull requests
+10
+Discussions
+Actions
+Projects
+Wiki
+Security
+Insights
+Settings
+.github/workflows/Automates.yml
+Update CI #14
+Jobs
+Run details
+Workflow file for this run
+.github/workflows/Automates.yml at 23a1ea3
+BEGIN
+GLOW4
+GLOW7
+#!/Usr/bin/Bash/ env :
+GLOW4
+Request :Pull
+Pulls :pull_requests :#!/https://github.com/mowjoejoejoejoe/docs/compare/main...0-wiz-0:docs:TingluoHuang@Patch 2
+BEGIN'
+GLOW4'
+checkout ':'#'Checks'-out ':via '::'#'Coommand.line :'' :
+ Check failure on line 10 in .github/workflows/Automates.yml
+
+GitHub Actions
+/ .github/workflows/Automates.yml
+Invalid workflow file
+You have an error in your yaml syntax on line 10
+If the conflicts on this branch are too complex to resolve in the web editor, you can check it out via command line to resolve the conflicts.
+https://github.com/mowjoejoejoejoe/WORKSFLOW.git
+Step 1: Clone the repository or update your local repository with the latest changes.
+git pull origin main
+Step 2: Switch to the head branch of the pull request.
+git checkout Master
+Step 3: Merge the base branch into the head branch.
+git merge main
+Step 4: Fix the conflicts and commit the result.
+See Resolving a merge conflict using the command line for step-by-step instructions on resolving merge conflicts.
+Step 5: Push the changes.
+git push -u origin Master
+https://github.com/mowjoejoejoejoe/docs/compare/main...0-wiz-0:docs:TingluoHuang-patch-1
+"dockerfile"::':Build::Publish ::
+Issues.tmp :parameters.md :
+\@@ -0,0 +1,2 @@
+# WORKSFLOW
+ AUTOMATE AUTOMATES BEGIN GLOW4 AUTOMATES#Test :tests :tests :Run'@ci: 
+Actions
+Projects
+4
+Security
+Insights
+Add automated spell check #17674
+jdkato started this conversation in Ideas
+Add automated spell check
+#17674
+@jdkato
+jdkato
+on May 6, 2022 · 3 comments
+Return to top
+Discussion options
+
+jdkato
+on May 6, 2022
+Hi,
+
+I'm the maintainer of Vale, a linter for prose that allows users to implement custom quality-assurance checks (spell check, repeated words, capitalization, etc.) for their documentation.
+
+One of the challenges of implementing Vale (especially with Markdown content) is getting it to correctly handle what I call "non-standard" markup: templating, shortcodes, snippets, and so on. To make this easier, I'm in the process of creating reusable configurations (or, "packages") for common static site generators and template engines, and I chose this repo as a test case for the Liquid package.
+
+Findings
+The following are a few of the errors I found while testing the package. I aired on the side of caution while going through the results; I ignored cases like "collectd" / "Collectd" (casing inconsistencies) and "filesize" / "file size" (spacing inconsistencies), among others.
+
+You can find the full list of cases that I ignored below.
+
+accept.txt (ignored cases)
+(?i)addassigneestoassignable
+(?i)addlabelstolabelable
+(?i)api
+(?i)bluetooth
+(?i)codeql
+(?i)cpp
+(?i)cpu
+(?i)crt
+(?i)css
+(?i)dev
+(?i)docx
+(?i)env
+(?i)facebook
+(?i)gc
+(?i)gifs?
+(?i)github
+(?i)gpg
+(?i)graphql
+(?i)gz
+(?i)html
+(?i)https?
+(?i)jpg
+(?i)js
+(?i)json
+(?i)mov
+(?i)nasa
+(?i)npm
+(?i)nvm
+(?i)pdf
+(?i)php
+(?i)png
+(?i)pptx
+(?i)rdoc
+(?i)rsa
+(?i)rss
+(?i)scim
+(?i)sha
+(?i)svg
+(?i)svn
+(?i)tmp
+(?i)todo
+(?i)toml
+(?i)tsv
+(?i)tv
+(?i)txt
+(?i)ubuntu
+(?i)uri
+(?i)vmg
+(?i)vue(:js)?
+(?i)wifi
+(?i)xlsx
+(?i)xml
+(?i)yaml
+(?i)yml
+[aA]llowlist(?:ing)?
+[aA]utogenerated
+[aA]utolink(?:s|ed)?
+[cC]odespaces?
+[cC]ollectd
+[eE]lasticsearch
+[eE]xfiltrating
+[gG]radle
+[hH]ubcat
+[iI]denticon
+[jJ]inja
+[kK]ustomize
+[lL]inux
+[lL]ucene
+[mM]icrosoft
+[mM]ultiline
+[oO]cto(?:kit|cat)?
+[pP]ageviews
+[pP]arallelization
+[pP]re
+[pP]roxying
+[pP]seudonymized
+[rR]ebased?
+[rR]ebasing
+[rR]erequest
+[rR]evokable
+[rR]ostered
+[sS]everability
+[sS]trikethrough
+[sS]ubprocessing
+[sS]ubprocessor
+[sS]udo
+[tT]akedown
+[tT]ransclusion
+[tT]riaging
+[uU]ncomment
+[uU]nfollow
+[uU]nhid(?:e|ing)
+[uU]npublishing
+[uU]nresolving
+[uU]nretire
+[uU]nreviewed
+[uU]nstar
+[uU]nsuspend
+[uU]ntrusted
+[uU]nverify
+[uU]nversioned
+[uU]nwatch
+abcd
+adminable
+Adoptium
+Affero
+Aland
+alia
+anonymized
+approvers?
+archivability
+Arq
+asc
+Atlassian
+attrs
+auditlog
+Authy
+autobuild
+Autoconf
+autodetect
+Autofetch
+autofills?
+autograding
+automations
+Automattic
+autoscalers?
+autoscaling
+backfilled
+Bazel
+becca
+benbalter
+benefitting
+benjaminp
+bert
+blockchain
+bolding
+bolton
+boolean
+bootcamps?
+boto
+botocore
+Braintree
+brettcannon
+brower
+Bundler
+Caicos
+callout
+Camo
+captcha
+Carbonite
+Carlini
+carrotsearch
+certifi
+chardet
+chroots?
+Citrix
+Clearbit
+Cliquez
+Cobertura
+codeblock
+codec
+Codium
+combatting
+committish
+composable
+configs?
+cppy
+cpython
+cron
+crontab
+cryptocurrency
+cryptomining
+csproj
+cybercrime
+datacenters?
+datastores?
+datetime
+dateutil
+deallocate
+deauthorized
+deauthorizes
+decompile
+dedent
+defunkt
+deidentify
+deliverables?
+demianbrecht
+demilestoned
+Dependabot
+deprovision
+deprovisioned
+dereferenced?
+discoverability
+dmnfarrell
+Dockerfile
+dotenv
+dotfiles?
+dotnet
+downscope
+downvoting
+Eloqua
+enablement
+enthought
+enums?
+exfiltrated?
+failover
+fairyfloss
+fakeaction
+fastai
+fayland
+filepaths?
+filesize
+Fixup
+fnmatch
+forkable
+formatters
+fpco
+fprime
+friedman
+frontmatter
+fullname
+futbol
+Gazit
+gcloud
+Gebru
+Gemfiles?
+gemrc
+geo
+gh
+gidgethub
+gists?
+gitattributes?
+gitk
+gjtorikian
+gollum
+Grafana
+Gravatar
+hardcode
+hardcoded
+hardcoding
+Hashicorp
+Hashimoto
+hh
+Homebrew
+hostname
+hotpackage
+hotpatch
+hotpatching
+hovercards?
+hppc
+Hubot
+Hyperscan
+iana
+ianare
+iceland
+ici
+idna
+ies
+infringers?
+innersource
+innersourcing
+interdependencies
+isor
+iss
+itsdangerous
+jaeteekae
+Janky
+JavaScript
+jemoji
+Jira
+jkeylu
+jlord
+jmespath
+Joomla
+journalctl
+jpaugh
+jq
+jts
+kanban
+kansas
+Kbuild
+kenya
+keycutter
+keyfiles?
+keypair
+kjd
+kohsuke
+kramdown
+Kumiko
+Laravel
+launchd
+learnings
+legislations
+Liberapay
+libsaas
+Libsodium
+licensors?
+licensure
+locationtech
+lockfiles?
+lodash
+Logstash
+london
+lowercased
+Luhansk
+lxml
+Macports
+Mada
+madler
+Mailgun
+Majeure
+Maki
+manpage
+markupsafe
+matplotlib
+matrixes
+mdo
+Memcache
+menubar
+mergeability
+mergeable
+metacpan
+michael
+middleware
+milestoned
+Miniconda
+minio
+Mmmmm
+monospaced?
+Moodle
+mozilla
+multithread
+multithreaded
+myorg
+Nagios
+nameservers?
+namespace
+nano
+nanoc
+nat
+nbviewer
+Nexmo
+Nginx
+ngrok
+noreply
+nuget
+numpy
+nuspec
+oauth
+obtenir
+Okta
+onboarded
+onboarding
+onwards
+opencv
+openssh
+Otechie
+overconsumption
+passwordless
+Patreon
+perl
+pexpect
+philschatz
+pipenv
+Pithub
+pksunkara
+plaintext
+plu
+pluggy
+Pluralsight
+postsecondary
+PowerPoint
+PowerShell
+Probot
+programatically
+protectable
+pseudonymisation
+ptyprocess
+publickey
+py
+Pygments
+pypa
+pyparsing
+pyserial
+pytest
+pytz
+pyyaml
+qlpacks?
+qmake
+Quickstarts
+rauhryan
+Ravi
+Raynes
+rb
+readme
+realtime
+reauthenticate
+reblame
+recategorized
+redactions?
+redcarpet
+redirections
+Redis
+reimplement
+reinstatements?
+remediations
+renderable
+Replit
+repls
+repo
+reselecting
+Resque
+resync
+retargeted
+retrievability
+retrigger
+reusability
+roadmaps?
+rollout
+rsync
+Rubocop
+Rubygems
+runtimes?
+russia
+sanitization
+scipy
+sdepold
+semver
+Sendgrid
+serverless
+setoff
+severities
+shipit
+shortcode
+shumway
+signup
+signups
+simplestyle
+Smee
+softprops
+spammy
+sponsorable
+src
+Statsd
+stringifying
+styleguide
+subcommand
+subfields?
+subfolders?
+sublicensable
+sublicense
+subnetworks?
+subprocesors?
+subprocessor
+subprojects?
+substring
+subtrees?
+syntaxes
+syslog
+tada
+Tailscale
+tdunning
+teardown
+Termux
+textarea
+textbox
+themself
+tibdex
+Tidelift
+Tiferet
+timeframe
+timestamping
+tkintertable
+toolchains?
+tooltips?
+torvalds
+traceroute
+transformative
+treemap
+triaged
+twbs
+Twilio
+typeaheads
+Typora
+tz
+uiri
+unactionable
+unarchived
+unarchives?
+unarchiving
+unassign
+uncurated
+unencoded
+unencrypted
+unescaped
+unicode
+Unlicense
+unlink
+unmark
+unmarking
+unmerged
+unminimized
+unpatched
+unpause
+unpublish
+unpublished
+unpushed
+unredacted
+unresolve
+unsave
+unscoped
+unselect
+unstash
+unstashing
+untrack
+untracked
+updatepullrequest
+updatepullrequest
+updatePullRequest
+upscaled
+upvoted
+urls?
+usr
+validator
+vendored
+vendoring
+Vijzelstraat
+vimrc
+von
+vscode
+Waf
+waitlist
+walkthrough
+wanstrath
+webcrawlers?
+webos
+webpages?
+werkzeug
+wget
+whistleblower
+whiteinge
+whitepaper
+Wiet
+wireframe
+wonkiness
+www
+Xcode
+Zendesk
+Zenodo
+zlib
+Zsh
+Zuora
+Error	Correction	Location
+from from	from	53:246
+secert	secret	78:230
+the the	the	45:256
+be be	be	28:212
+worflows	workflows	206:86
+Luhasnk	Luhansk	51:45
+transfering	transferring	535:154
+Nothwithstanding	Notwithstanding	287:70
+apprioriate	appropriate	121:283
+currenly	currently	23:259
+vulnerabilites	vulnerabilities	43:36
+organizaton	organization	213:136
+offical	official	29:39
+Commiters	Committers	100:143
+Configuration
+If you'd like to add Vale to this repo, here's the configuration I used:
+
+StylesPath = .github/styles
+MinAlertLevel = suggestion
+
+# The accept.txt file is given above
+Vocab = Base
+
+SkippedScopes = script, style, pre, figure, alt, strong
+
+[*.md]
+BasedOnStyles = Vale
+
+TokenIgnores = ({%.+?%}), ({{.+?}})
+
+BlockIgnores = (?s) *({% raw %}\n.*?\n{% endraw %})
+This generally worked pretty good. There were still a few edge cases that I needed to disabled Vale on (using <!-- vale off --> comments), such as the tables in migrating-from-circleci-to-github-actions.md.
+
+I understand if this isn't something you're interested in, but I figured I'd share my work just in case.
+
+You can't vote on a locked discussion
+sarahs reacted with heart emoji
+Replies:3 comments
+Comment options
+
+ramyaparimi
+on May 9, 2022
+@jdkato Thanks so much for sharing your thoughts in here. We have been trying to implement Vale on this repo since quite some time. We faced a few blockers owing to the challenges while handling non standard markup situations. We raised raised this and this issue in the past with the challenges we had.
+
+With your suggestion, I am currently using TokenIgnores to ignore liquids during linting. The use of regex in BlockedIgnores is something I haven't tried so I am going to check out your configuration.
+
+Thanks so much for choosing the GitHub docs repo as a test for your use case. I will get back to you once I test this and discuss with the team 💖 .
+
+You can't vote on a locked discussion
+shukla2004 reacted with thumbs up emoji
+0 replies
+Comment options 
+:Build::
+Add :Automates.yml/spell-check.yml :
+Update CI · mowjoejoejoejoe/WORKSFLOW@23a1ea3usr/bin/.sh env :
 BEGIN
 GLOW4
 Name :buildthen-deployee-title: 	ci :C:\zieskr/repositories/bitore.sig env
